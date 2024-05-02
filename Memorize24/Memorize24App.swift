8691b43f153326aa797e7ade1e797c6953b734d6
Roy Berner <r.p.berner@me.com>
2024-05-02 20:15:39 +0200

    Initial Commit

---
+++ Memorize24/Memorize24App.swift
@@ -0,0 +1,17 @@
+//
+//  Memorize24App.swift
+//  Memorize24
+//
+//  Created by Roy Berner on 02/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Memorize24App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Memorize24UITests/Memorize24UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Memorize24UITestsLaunchTests.swift
+//  Memorize24UITests
+//
+//  Created by Roy Berner on 02/05/2024.
+//
+
+import XCTest
+
+final class Memorize24UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
